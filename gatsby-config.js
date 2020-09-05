43d79de871a12c345d553e0aafaa9c9d20f06007
Kyle Starr <kylestarr@Kyles-13-inch-MacBook-Pro.local>
2020-09-05 13:06:21 -0700

    updating title, renaming 'master' branch to 'main'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Zero Counts`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Kyle Starr`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
