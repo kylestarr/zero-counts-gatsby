bfb16fc24a284e19c2d383896af919529a735639
Kyle Starr <kyle.starr@icloud.com>
2020-09-06 15:10:10 -0700

    adding pagination

---
+++ src/templates/blog-list.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+
+import SEO from '../components/seo'
+import Layout from '../components/layout'
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+    const { currentPage, numPages } = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString()
+    const nextPage = (currentPage + 1).toString()
+    
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={siteTitle}
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3>
+                <Link to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+            <p
+                dangerouslySetInnerHTML={{
+                  __html: node.html,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+      {!isFirst && (
+        <Link to={`../${prevPage}`} rel="prev">
+          ← Previous Page
+        </Link>
+      )}
+      {!isLast && (
+        <Link to={`../${nextPage}`} rel="next">
+          Next Page →
+        </Link>
+      )}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query blogPageQuery($skip: Int!, $limit: Int!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          html
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
