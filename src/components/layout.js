7d119db95e8a854e565def6d7dfaabbf31493dbc
Kyle Starr <kyle.starr@icloud.com>
2020-09-13 07:52:43 -0700

    layout tweaks

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,25 @@ const Layout = ({ title, children }) => {
   return (
     <div>
       <div class="row header">
-        <div class="col-xs-0 col-md-3 col-lg-3"></div>
-        <div class="col-xs-12 col-md-6 col-lg-6">
+        <div class="col-xs-0 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
           <Header />
         </div>
-        <div class="col-xs-0 col-md-3 col-lg-3"></div>
+        <div class="col-xs-0 col-sm-2 col-md-2 col-lg-3"></div>
       </div>
       <div class="row">
-        <div class="col-xs-0 col-md-2 col-lg-3"></div>
-        <div class="col-xs-12 col-md-8 col-lg-6">
+        <div class="col-xs-0 col-sm-2 col-md-2 col-lg-3"></div>
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
           <main>{children}</main>
         </div>
-        <div class="col-xs-0 col-md-2 col-lg-3"></div>
+        <div class="col-xs-0 col-sm-2 col-md-2 col-lg-3"></div>
       </div>
       <div class="row footer">
-        <div class="col-xs-0 col-md-3 col-lg-3"></div>
-        <div class="col-xs-12 col-md-8 col-lg-6">
+        <div class="col-xs-0 col-sm-3 col-md-2 col-lg-3"></div>
+        <div class="col-xs-12 col-sm-6 col-md-8 col-lg-6">
           <Footer />
         </div>
-        <div class="col-xs-0 col-md-3 col-lg-3"></div>
+        <div class="col-xs-0 col-sm-3 col-md-2 col-lg-3"></div>
       </div>
     </div>
   )
