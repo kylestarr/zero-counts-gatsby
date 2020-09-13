8a779923b127cd265f970ae1c248db116dc42082
Kyle Starr <kyle.starr@icloud.com>
2020-09-12 19:34:38 -0700

    updating mobile layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,11 +21,11 @@ const Layout = ({ title, children }) => {
         <div class="col-xs-0 col-md-2 col-lg-3"></div>
       </div>
       <div class="row footer">
-        <div class="col-xs-1 col-md-3 col-lg-3"></div>
-        <div class="col-xs-10 col-md-8 col-lg-6">
+        <div class="col-xs-0 col-md-3 col-lg-3"></div>
+        <div class="col-xs-12 col-md-8 col-lg-6">
           <Footer />
         </div>
-        <div class="col-xs-1 col-md-3 col-lg-3"></div>
+        <div class="col-xs-0 col-md-3 col-lg-3"></div>
       </div>
     </div>
   )
