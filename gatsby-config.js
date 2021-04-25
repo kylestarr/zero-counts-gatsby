d2b5458a83b91be1d3af8ca7f114901e87005c87
Kyle Starr <kyle.starr@icloud.com>
2021-04-25 08:55:14 -0700

    adding gatsby-plugin-gatsby-cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,8 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     'gatsby-plugin-meta-redirect',
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-gatsby-cloud`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
