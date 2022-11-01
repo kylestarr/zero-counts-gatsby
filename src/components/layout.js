82604584c3b1b35737b8c20b81301ca0db570f20
Kyle Starr <kyle.starr@icloud.com>
2022-11-01 10:17:53 -0700

    adding game boy mods nav item

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,6 +18,7 @@ const Layout = ({ title, children }) => {
           <div class="col-xs-0 col-sm-2 col-md-2 col-lg-3"></div>
           <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
             <Link to={"/archive"}>Archive</Link>
+            <Link to={"/gameboys"}>Game Boy Mods</Link>
           </div>
           <div class="col-xs-0 col-sm-2 col-md-2 col-lg-3"></div>
         </div>
