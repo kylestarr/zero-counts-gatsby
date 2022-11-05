ea36ff94d3dc06042bc7d4375285a22a659f9200
Kyle Starr <kyle.starr@icloud.com>
2022-11-05 15:09:48 -0700

    prettier cleanup

--- src/templates/gameboys-list.js
+++ src/templates/gameboys-list.js
@@ -1,49 +1,50 @@
-import React from 'react'
-import { Link, graphql } from 'gatsby'
+import React from "react"
+import { Link, graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-import SEO from '../components/seo'
-import Layout from '../components/layout'
+import SEO from "../components/seo"
+import Layout from "../components/layout"
 
 class GameBoysIndex extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
-    
+
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title={"Game Boy Modding Gallery"}
-          keywords={[`video games`, `blog`, `business`, `education`, `culture`, `design`]}
+          keywords={[
+            `video games`,
+            `blog`,
+            `business`,
+            `education`,
+            `culture`,
+            `design`,
+          ]}
           thumbnail={`/game-boy-gallery-hero.jpeg`}
           thumbnailAlt={"Three modded Game Boys"}
         />
         <div id="gameboys-list">
-            {
-                posts.map(({ node }) => {
-                    const title = node.frontmatter.title
-                    return (
-                      <div class="article">
-                        <Link to={node.frontmatter.slug}>
-                        
-                          <article key={node.frontmatter.slug}>
-                            <div class="article-thumbnail">
-                                <GatsbyImage
-                                  image={getImage(node.frontmatter.thumbnail)}
-                                  alt={node.frontmatter.title}
-                                />
-                            </div>
-                            <h2>
-                              {title}
-                            </h2>
-                          </article>
-                        
-                        </Link>
-                      </div>
-                    ) 
-                })
-            }
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title
+            return (
+              <div class="article">
+                <Link to={node.frontmatter.slug}>
+                  <article key={node.frontmatter.slug}>
+                    <div class="article-thumbnail">
+                      <GatsbyImage
+                        image={getImage(node.frontmatter.thumbnail)}
+                        alt={node.frontmatter.title}
+                      />
+                    </div>
+                    <h2>{title}</h2>
+                  </article>
+                </Link>
+              </div>
+            )
+          })}
         </div>
       </Layout>
     )
@@ -60,23 +61,23 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-        sort: { fields: [frontmatter___date], order: DESC }
-        filter: {fileAbsolutePath: {regex: "/gameboys/"}}
-        ) {
-        edges {
-            node {
-              frontmatter {
-                title
-                date(formatString: "MMMM DD, YYYY")
-                thumbnail {
-                  childImageSharp {
-                    gatsbyImageData
-                  }
-                }
-                slug
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fileAbsolutePath: { regex: "/gameboys/" } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
               }
             }
+            slug
+          }
         }
+      }
     }
   }
 `
