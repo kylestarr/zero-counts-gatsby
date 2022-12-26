76c8ed3e41adc9bd09b875b3ba7689db7a9966c7
Kyle Starr <kyle.starr@icloud.com>
2022-12-26 15:44:07 -0800

    reverting cloud config

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,7 @@ module.exports = {
       resolve: `gatsby-plugin-gatsby-cloud`,
       options: {
         headers: {
-          "/rss.xml": ["content-type: application/rss+xml"],
+          "/rss.xml": ["Content-Type: application/rss+xml"],
         },
       },
     },
